410cd94545a46c66a6e5ab6b52cc4903916cbdf5
Gama19999 <gama.rios0110@gmail.com>
2024-10-22 12:24:53 -0600

    Version 1.0.3
    AppLinkPath bug fix

--- src/android/ovh/serial30/cordova/applinks/pojos/AppLinkPath.java
+++ src/android/ovh/serial30/cordova/applinks/pojos/AppLinkPath.java
@@ -8,7 +8,7 @@ public class AppLinkPath {
     private final String pathURL;
     private final String jsEvent;
 
-    public DeepLinkPath(final String pathURL, final String jsEvent) {
+    public AppLinkPath(final String pathURL, final String jsEvent) {
         this.pathURL = pathURL.replace("*", "(.*)").toLowerCase();
         this.jsEvent = jsEvent;
     }
